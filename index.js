e617c320ee467babeb92d7ca5b9d7a7faacc761b
Shakawat Hossain Sadik <saadiq9x@gmail.com>
2026-06-07 08:12:18 +0600

    First Commit

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Server has started");
+});
+
+const run = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    console.log("Connected to MongoDB");
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+
+    const dataCol = client.db("Wonderlast").collection("allThatRemains");
+
+    app.post("/destinations", async (req, res) => {
+        const newDestination = req.body;
+        console.log(newDestination);
+
+        const result = await dataCol.insertOne(newDestination);
+
+        res.json(result);
+    })
+
+    app.get("/destinations", async (req, res) => {
+
+        const result = await dataCol.find().toArray();
+
+        res.send(result);
+    })
+
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+};
+
+run()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch(console.dir);
