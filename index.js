8c36b6249c7b64e7dc2a3964467ab1b8347f0a41
Shakawat Hossain Sadik <saadiq9x@gmail.com>
2026-06-07 23:38:37 +0600

    Continuous Commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient, ServerApiVersion } from "mongodb";
+import { ObjectId } from "mongodb";
 
 dotenv.config();
 const app = express();
@@ -37,20 +38,73 @@ const run = async () => {
     const dataCol = client.db("Wonderlast").collection("allThatRemains");
 
     app.post("/destinations", async (req, res) => {
-        const newDestination = req.body;
-        console.log(newDestination);
+      const newDestination = req.body;
+      console.log(newDestination);
 
-        const result = await dataCol.insertOne(newDestination);
+      const result = await dataCol.insertOne(newDestination);
 
-        res.json(result);
-    })
+      res.json(result);
+    });
 
     app.get("/destinations", async (req, res) => {
+      const result = await dataCol.find().toArray();
 
-        const result = await dataCol.find().toArray();
+      res.send(result);
+    });
 
-        res.send(result);
+    app.get("/destinations/:id", async (req, res) => {
+      const {id} = req.params;
+      console.log(req.params)
+      const result = await dataCol.find(
+        {
+            _id: new ObjectId(id)
+        }
+      ).toArray();
+
+      res.send(result);
+    });
+
+    app.patch("/destinations/:id", async (req, res) => {
+        const {id} = req.params;
+        const editedDestination = req.body;
+        const result = await dataCol.updateOne (
+            {
+                _id: new ObjectId(id)
+            },
+            {
+                $set: {
+                    editedDestination
+                }
+            }
+        )
     })
+    //==========================================================================
+
+    app.patch("/destinations", async (req, res) => {
+      try{
+        const change = dataCol.updateMany(
+        {
+          "workspace-description": { $exists: true },
+        },
+        {
+          $rename: { "workspace-description": "description" },
+        },
+      );
+
+      await res.json({
+        message:"Updated Successfully",
+        modifiedCount: change.modifiedCount
+      })
+      } catch (e){
+        console.error(e);
+        res.status(500).json({ error: "Failed to update documents" });
+      }
+    });
+    /*
+    Then run `curl -X PATCH http://localhost:5000/destinations` in bash terminal.
+    If succeeded, bash will return `{"message":"Updated Successfully"}`.
+    Easy peasy, no PostMan required
+    */
 
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
